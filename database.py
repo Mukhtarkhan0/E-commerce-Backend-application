be7465cb09fe2b09e8de63b331686769dd8758f1
Mukhtarkhan0 <mukhtarkha003@gmail.com>
2025-07-22 01:09:50 +0530

    initial commit

--- database.py
+++ database.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-MONGODB_URI = os.getenv("MONGODB_URI")
-
+MONGODB_URI = "mongodb+srv://mukhtar:1234@hronetaskmk.risvnox.mongodb.net/?retryWrites=true&w=majority&appName=HROneTaskMK"
 client = MongoClient(MONGODB_URI)
 db = client["ecommerce"]
 products_collection = db["products"]
